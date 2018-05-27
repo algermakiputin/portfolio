8a8148bf17c565afca653bbf81d764a5a2ac2859
Alger Makiputin <algerzxc@gmail.com>
2018-05-27 21:28:56 +0800

    init

---
+++ Assets/js/index.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('1000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+
+        if ($(".filter-button").removeClass("active")) {
+            $(this).removeClass("active");
+        }
+        $(this).addClass("active");
+    });
+    
+    skillbars();
+});
+
+function skillbars () {
+
+
+    $('#s-html').simpleSkillbar({
+
+            width: 85 // 80%,
+    });
+
+    $('#s-css').simpleSkillbar({
+ 
+            width: 90 // 80%
+    });
+
+    $('#s-js').simpleSkillbar({
+ 
+            width: 80 // 80%
+    });
+
+    $('#s-php').simpleSkillbar({
+ 
+            width: 88 // 80%
+    });
+
+    $('#s-wordpress').simpleSkillbar({
+ 
+            width: 80 // 80%
+    });
+
+    $('#s-seo').simpleSkillbar({
+ 
+            width: 75 // 80%
+    });
+
+    $('#s-codeigniter').simpleSkillbar({
+ 
+            width: 90 // 80%
+    });
+
+    $('#s-laravel').simpleSkillbar({
+ 
+            width: 78 // 80%
+    });
+}
