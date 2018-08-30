0409c4a1d370bcea1570ad9d27e97c346e39a202
Alger Makiputin <algerzxc@gmail.com>
2018-08-30 13:22:35 +0800

    remove google map

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,4 @@
 
-// google map
-var map = '';
-var center;
-
 function initialize() {
     
     
@@ -73,16 +69,6 @@ function counter(top,win,started) {
     
 }
 
-function calculateCenter() {
-  center = map.getCenter();
-}
-
-function loadGoogleMap(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-    document.body.appendChild(script);
-}
 
 // Flexslider
 $(function(){
@@ -103,7 +89,7 @@ $(function(){
     }    
   });
 
-  loadGoogleMap();
+ 
 });
 
 // isotope
