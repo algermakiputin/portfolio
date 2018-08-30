82195cf29bbbb47a76400d970f3a16c67e1549e5
Alger Makiputin <algerzxc@gmail.com>
2018-08-30 13:38:34 +0800

    js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,8 @@
 
+// google map
+var map = '';
+var center;
+
 function initialize() {
     
     
@@ -69,6 +73,14 @@ function counter(top,win,started) {
     
 }
 
+ 
+
+function loadGoogleMap(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
 
 // Flexslider
 $(function(){
@@ -89,7 +101,7 @@ $(function(){
     }    
   });
 
- 
+  loadGoogleMap();
 });
 
 // isotope
