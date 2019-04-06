d4c96d2775b983e11096bba55a7f3fb2008eafd6
alger makiputin <algerzxc@gmail.com>
2019-04-06 15:26:04 +0800

    a

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+// import 'css/animate.min.css';
+// import 'css/bootstrap.min.css';
+// import 'css/magnific-popup.css';
+// import 'css/font-awesome.min.css';
+// import 'css/style.css';
+module.exports = {
+  
+}
+class MyApp extends React.Component {
+
+  render () {
+    return(
+        <h1>Hello, World!</h1>
+      )
+  }
+}
+
+ReactDOM.render(<MyApp />,document.getElementById('root'));
